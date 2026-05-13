664bbca823a6cfceed9a84527ebe9bf1277d6ee9
seddiq1 <seddiq.daoudi@galileiostiglia.edu.it>
2026-05-13 09:18:45 +0200

    Implement loan management functions in prestiti.c

---
+++ include/prestiti.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/prestiti.h"
+
+void inizializza_coda(CodaNotifiche *q) {
+    q->fronte = q->retro = NULL;
+}
+
+void aggiungi_notifica(CodaNotifiche *q, char *msg) {
+    NodoNotifica *nuovo = malloc(sizeof(NodoNotifica));
+    strcpy(nuovo->messaggio, msg);
+    nuovo->next = NULL;
+    if (q->retro == NULL) {
+        q->fronte = q->retro = nuovo;
+    } else {
+        q->retro->next = nuovo;
+        q->retro = nuovo;
+    }
+}
+
+void crea_prestito() {
+    int id_u, id_l;
+    printf("ID Utente: "); scanf("%d", &id_u);
+    printf("ID Libro: "); scanf("%d", &id_l);
+
+    Utente *u = cerca_utente_id(id_u);
+    Libro *l = cerca_libro_id(id_l);
+
+    if (u && l && l->copie_disponibili > 0) {
+        Prestito *p = malloc(sizeof(Prestito));
+        p->utente = u;
+        p->libro = l;
+        p->data_inizio = time(NULL);
+        p->data_scadenza = p->data_inizio + (30 * 24 * 60 * 60); // +30 giorni
+        p->restituito = 0;
+        
+        l->copie_disponibili--;
+        l->totale_prestiti++;
+
+        // Aggiunta alla lista collegata dell'utente
+        NodoPrestito *nuovoNodo = malloc(sizeof(NodoPrestito));
+        nuovoNodo->dati_prestito = p;
+        nuovoNodo->next = u->prestiti_attivi;
+        u->prestiti_attivi = nuovoNodo;
+
+        printf("Prestito registrato con successo!\n");
+    } else {
+        printf("Errore: Utente/Libro non trovato o copie esaurite.\n");
+    }
+}
+
+void visualizza_notifiche(CodaNotifiche *q) {
+    if (q->fronte == NULL) return;
+    printf("[NOTIFICHE SCADENZE]\n");
+    while (q->fronte != NULL) {
+        NodoNotifica *temp = q->fronte;
+        printf("- %s\n", temp->messaggio);
+        q->fronte = q->fronte->next;
+        free(temp);
+    }
+    q->retro = NULL;
+}
